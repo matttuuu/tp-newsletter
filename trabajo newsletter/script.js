f8135a90f8b2460625dd126c97da3239dd458de6
Mateo Cordoba <matucorneron@gmail.com>
2024-08-13 15:27:39 -0300

    Agregar archivos finales

--- trabajo newsletter/script.js
+++ trabajo newsletter/script.js
@@ -1,7 +1,33 @@
-/**/
+const expRegulares = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
-alert("Buen dia grupo")
+const formulario = document.getElementById('formulario-mail');
+const campoInput = document.querySelector('#formulario-mail input');
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevenir el envío del formulario para hacer la validación
+
+    const inputValue = campoInput.value; // Obtener el valor ingresado por el usuario
     
+});
+
+
+function validateEmail(inputValue) { 
+    let isValid = expRegulares.test(inputValue);
+
+    if(inputValue === ''){
+        alert("El campo está vacio; por favor, ingrese un correo electronico valido");
+        return;
+    }
+
+
+    if (isValid) {
+        alert("Dir de correo: " + inputValue + " registrada; ¡Gracias por elegirnos!");
+    } else {
+        alert("El correo electrónico no es válido, por favor ingrese otro.");
+    }
+}
+
+
 
 
 
